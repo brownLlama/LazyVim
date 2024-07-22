6799b741c2c57b995e58571dc5e1276abd18f270
brownLlama <inbox.sanu@gmail.com>
2024-07-22 20:43:59 +0200

    keymaps updated

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,11 +4,6 @@
 
 vim.keymap.del("n", "<leader>K")
 vim.keymap.del("n", "<leader>L")
---
--- vim.keymap.del("n", "<leader>,")
--- vim.keymap.del("n", "<leader>:")
--- vim.keymap.del("n", "<leader> ")
---
 vim.keymap.del("n", "<leader>-")
 vim.keymap.del("n", "<leader>|")
 vim.keymap.del("n", "<leader>?")
@@ -16,3 +11,10 @@ vim.keymap.del("n", "<leader>`")
 vim.keymap.del("n", "<leader>qq")
 vim.keymap.del("n", "<leader>xl")
 vim.keymap.del("n", "<leader>xq")
+
+vim.keymap.del("n", "<leader>bb")
+vim.keymap.del("n", "<leader>bD")
+vim.keymap.del("n", "<leader>ft")
+vim.keymap.del("n", "<leader>fT")
+
+vim.keymap.del("n", "<leader>sc")
